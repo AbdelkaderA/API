1ff775c0bd349596b60e4f45460f0b9ba1a72b87
[VBLab] <58741067+FerrandezBenjamin@users.noreply.github.com>
2020-03-09 16:15:11 +0100

    Creat rempli

--- create.php
+++ create.php
@@ -1,6 +1,32 @@
 <?php
-include 'db_connect.php();
+
+include 'db_connect.php' ;
+
+$db = db_connect();
+
+$adresses = $_POST ["Adresses_idAdresses"];
+$utilisateur = $_POST ["Utilisateur_idUser"];
+$produitCmd = $_POST ["Produits_idProduits"];
+$quantite = $_POST ["Quantité"];
 
 
+$query = 
+"INSERT INTO commande (Adresses_idAdresses, Utilisateur_idUser) 
+VALUES (".$adresses.",".$utilisateur.")";
+
+$commd = $db -> prepare($query);
+$commd -> execute();
+
+$derncommd= $db -> query('SELECT idCommande FROM commande ORDER BY idCommande DESC LIMIT 1');
+
+$dernid = $derncommd -> fetch();
+
+
+$pcommd =
+"INSERT INTO produitscommande(Commande_idCommande,Produits_idProduits,Quantité,EtatLivraison)  
+VALUES (".$dernid['idCommande'].",".$produitCmd.",".$quantite.",\"préparation\")";
+
+$insertion = $db -> prepare($pcommd);
+$insertion -> execute();
     
 ?>

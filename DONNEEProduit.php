b9a93f42dab2532edd65255beb2257d60fc8e0ae
UlysC <ulys.chambard@le-campus-numerique.fr>
2020-03-10 12:51:37 +0100

    corrected donne

--- DONNEEProduit.php
+++ DONNEEProduit.php
@@ -3,7 +3,7 @@
 function db_connect($dbname, $user, $pass){  // La fonction pour se connecter à la base de donnée
          
                 try{
-                    $db = new PDO('mysql:host=localhost;dbname='.$dbname.';port=3306', $user, $pass);
+                    $db = new PDO('mysql:host=localhost;dbname='.$dbname.';port=3308', $user, $pass);
                     }
                 catch(Exception $e){
                     $err = $e->getMessage();
@@ -15,14 +15,10 @@ function db_connect($dbname, $user, $pass){  // La fonction pour se connecter à
 $db = db_connect('testcommande','root',''); // On appelle la fonction et donc on se connecte à la bdd
 
 $quer = $db->query('SELECT * FROM produits WHERE Categorie="'.$_GET['categorie'].'"'); // On selectionne les produits selon leur categorie (dans variable GET, dans l'URL)
-
+$result = Array() ;
 while($data = $quer->fetch(PDO::FETCH_ASSOC )){ // Tant qu'on récupère des lignes (des produits)
-    var_dump($data);
-   header('Content-Type: application/json');  // On renvoie le code en JSON 
-    echo json_encode($data, JSON_PRETTY_PRINT);
-
+    $result[]=$data ;  
 } 
-
-
-
+header('Content-Type: application/json');  // On renvoie le code en JSON 
+echo json_encode($result, JSON_PRETTY_PRINT);
 ?>
